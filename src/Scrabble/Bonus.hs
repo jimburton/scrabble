4c0f00659cc5acf40a76b337bb3b29734565d872
James Burton <j.burton@brighton.ac.uk>
2021-03-29 10:55:14 +0100

    Chapter 1

---
+++ src/Scrabble/Bonus.hs
@@ -0,0 +1,55 @@
+{-|
+Module      : Scrabble.Bonus
+Description : Functions for bonuses in the scrabble library.
+Maintainer  : j.burton@brighton.ac.uk
+Stability   : experimental
+Portability : POSIX
+
+Contains the @bonusMap@ mapping positions to bonuses.
+-}
+module Scrabble.Bonus (bonusMap)
+  where
+
+import qualified Data.Map as Map
+import Data.Map (Map)
+import Scrabble.Types (Pos, Bonus(..))
+-- ============ Bonuses ============ --
+
+-- Data for the bonus map.
+bonusSquaresList :: [(Pos, Bonus)] -- ((Row, Column), Bonus)
+bonusSquaresList =
+  [((0, 0),    W3), ((0, 3),   L2)
+  , ((0, 7),   W3), ((0, 11),  L2)
+  , ((0, 14),  W3), ((1, 1),   W2)
+  , ((1, 5),   L3), ((1, 9),   L3)
+  , ((1, 13),  W2), ((2, 2),   W2)
+  , ((2, 6),   L2), ((2, 8),   L2)
+  , ((2, 12),  W2), ((3, 0),   L2)
+  , ((3, 3),   W2), ((3, 7),   L2)
+  , ((3, 11),  W2), ((3, 14),  L2)
+  , ((4, 4),   W2), ((4, 10),  W2)
+  , ((5, 1),   L3), ((5, 5),   L3)
+  , ((5, 9),   L3), ((5, 13),  L3)
+  , ((6, 2),   L2), ((6, 6),   L2)
+  , ((6, 8),   L2), ((6, 12),  L2)
+  , ((7, 0),   W3), ((7, 3),   L2)
+  , ((7, 7),   W2), ((7, 11),  L2)
+  , ((7, 14),  W3), ((8, 2),   L2)
+  , ((8, 6),   L2), ((8, 8),   L2)
+  , ((8, 12),  L2), ((9, 1),   L3)
+  , ((9, 5),   L3), ((9, 9),   L3)
+  , ((9, 13),  L3), ((10, 4),  W2)
+  , ((10, 10), W2), ((11, 0),  L2)
+  , ((11, 3),  W2), ((11, 7),  L2)
+  , ((11, 11), W2), ((11, 14), L2)
+  , ((12, 2),  W2), ((12, 6),  L2)
+  , ((12, 8),  L2), ((12, 12), W2)
+  , ((13, 1),  W2), ((13, 5),  L3)
+  , ((13, 9),  L3), ((13, 13), W2)
+  , ((14, 0),  W3), ((14, 3),  L2)
+  , ((14, 7),  W3), ((14, 11), L2)
+  , ((14, 14), W3)]
+
+-- | Map containing the bonus squares on the board.
+bonusMap :: Map Pos Bonus
+bonusMap = Map.fromList bonusSquaresList
---
+++ src/Scrabble/Pretty.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-|
+Module      : Scrabble.Pretty
+Description : Pretty printing of Scrabble boards.
+Maintainer  : j.burton@brighton.ac.uk
+Stability   : experimental
+Portability : POSIX
+
+Functions for pretty-printing a Scrabble board.
+-}
+module Scrabble.Pretty (showBoard)
+  where
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Map as Map
+import Data.Array
+import Data.List.Split (chunksOf)
+import Scrabble.Dict (letterToChar)
+import Scrabble.Bonus (bonusMap)
+import Scrabble.Types
+  ( Board
+  , Tile
+  , Letter )
+  
+-- ========== Printing the board ========== --
+
+-- Get all rows from the board
+rows :: Board -> [[Maybe Tile]]
+rows b = chunksOf 15 (elems b) 
+
+-- | Textify a board.
+showBoard :: Bool  -- ^ Whether to show bonus squares.
+          -> Board -- ^ The board.
+          -> Text  -- ^ The text representation of the board.
+showBoard printBonuses b = topNumbers <> top <> showRows <> bottom where
+  showRows      = T.intercalate "\n" (zipWith showRow [0..14] (rows b)) <> "\n"
+  showRow     i r = showI i <> "|" <>
+                    T.concat (zipWith (showSquare i) [0..14] r)
+  showSquare :: Int -> Int -> Maybe (Letter,Int) -> Text
+  showSquare i c s = case s of
+                       Nothing    ->
+                         if printBonuses
+                         then case Map.lookup (i,c) bonusMap of
+                                Nothing -> "  |"
+                                Just b' -> T.pack (show b') <> "|"
+                         else "  |"
+                       Just (t,_) -> T.pack [' ', letterToChar t, '|']
+  topNumbers    = "  |" <> T.concat (map (\i -> showI i <> "|") [0..14]) <> "\n"
+  showI         :: Int -> Text
+  showI i       = if i < 10 then " " <> T.pack (show i) else T.pack (show i)
+  top           = line '-'
+  bottom        = line '-'
+  line        c = T.pack (replicate 48 c) <> "\n"
